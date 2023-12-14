66693a0f50262be31a70c5c49dcd0d26a1188173
22-JWL <jck1004ee@gmail.com>
2023-12-15 01:42:47 +0900

    feat: test

--- src/app/chart/test/page.tsx
+++ src/app/chart/test/page.tsx
@@ -1,3 +1,4 @@
+import Item from '@/component/test';
 import './test.scss';
 export default function test() {
   let str: string = '차트 페이지';
@@ -10,18 +11,22 @@ export default function test() {
     <div>
       <h2 className="ChartTitle"> 이 페이지는 {str}입니다.</h2>
 
-      <div className="food">
-        <h4>상품 1 $40</h4>
-      </div>
-      <div className="food">
-        <h4>상품 2 $40</h4>
-      </div>
+      <InstrumentItem></InstrumentItem>
+      <Item></Item>
       <div className="food">
         <h4>상품 3 $40</h4>
       </div>
       <div className="food">
         <h4>상품 4 $40</h4>
       </div>
+    </div>
+  );
+}
+
+function InstrumentItem() {
+  return (
+    <div className="food">
+      <h4>상품 1 $40</h4>
     </div>
   );
 }
