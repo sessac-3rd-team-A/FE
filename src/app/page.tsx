0265c777b85c38a1c9f3461351914261cc594122
Sangwoo97 <aa6618@naver.com>
2023-12-15 14:20:43 +0900

    feat: 애니메이션 작업 중간저장 (#9)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from 'next/image';
+// import styles from './page.module.css'
+import '@/styles/main.scss';
 
 export default function Home() {
   return (
-    <div>hi~~~</div>
-  )
+    <div className="mainWindContainer">
+      <img className="mainWindImg" src="/mainBlow.png" alt="바람" />
+      <img className="mainSmileImg" src="/main_smile.png" alt="행복한얼굴" />
+    </div>
+  );
 }
