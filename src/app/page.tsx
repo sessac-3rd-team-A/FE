d95f42f1c031b196bc512e7863be41104278fad4
Sangwoo97 <aa6618@naver.com>
2023-12-15 16:36:01 +0900

    feat: 메인 중간저장 (#9)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
-import Image from 'next/image';
-// import styles from './page.module.css'
-import '@/styles/main.scss';
+import Main from '@/containers/main';
 
 export default function Home() {
-  return (
-    <div className="mainWindContainer">
-      <img className="mainWindImg" src="/mainBlow.png" alt="바람" />
-      <img className="mainSmileImg" src="/main_smile.png" alt="행복한얼굴" />
-    </div>
-  );
+  return <Main />;
 }
