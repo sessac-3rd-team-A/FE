2a399c9221c44bfb14c93874e2772960db9a82d0
sy33002 <sy33002@naver.com>
2023-12-15 19:16:45 +0900

    css: profile footer css 작업 중

--- src/app/profile/myShop/page.tsx
+++ src/app/profile/myShop/page.tsx
@@ -1,9 +1,11 @@
 import MyShopContainer from "@/containers/myShop";
+import ProfileFooter from "@/containers/common/profileFooter"
 
 export default function MyShop() {
   return(
     <>
       <MyShopContainer />
+      <ProfileFooter />
     </>
   )
 }
