356ba3b63841d729a26bb805a8d94e126c484e73
jjsh03 <aberrant03@gmail.com>
2023-12-16 14:09:31 +0900

    feat: sigh 작성-랜딩 페이지 제작(#17)

--- src/containers/Sigh/index.tsx
+++ src/containers/Sigh/index.tsx
@@ -1,3 +1,64 @@
+import '@/styles/sigh.scss';
+
 export default function SighPage() {
-  return <div>한숨 페이지입니당</div>;
+  return (
+    <div className="sighContainer">
+      <main className="sighMainContainer">
+        <h1 className="sighText">Let me hear your sigh</h1>
+        <button className="sighStart">START</button>
+      </main>
+      <div className="sighDoodles">
+        <img
+          src="/sigh/sigh_doodle_1.png"
+          alt="doodle"
+          className="doodle doodleA"
+        />
+        <img
+          src="/sigh/sigh_doodle_2.png"
+          alt="doodle"
+          className="doodle doodleB"
+        />
+        <img
+          src="/sigh/sigh_doodle_3.png"
+          alt="doodle"
+          className="doodle doodleC"
+        />
+        <img
+          src="/sigh/sigh_doodle_4.png"
+          alt="doodle"
+          className="doodle doodleD"
+        />
+        <img
+          src="/sigh/sigh_doodle_5.png"
+          alt="doodle"
+          className="doodle doodleE"
+        />
+        <img
+          src="/sigh/sigh_doodle_6.png"
+          alt="doodle"
+          className="doodle doodleF"
+        />
+        <img
+          src="/sigh/sigh_doodle_7.png"
+          alt="doodle"
+          className="doodle doodleG"
+        />
+        <img
+          src="/sigh/sigh_doodle_8.png"
+          alt="doodle"
+          className="doodle doodleH"
+        />
+        <img
+          src="/sigh/sigh_doodle_9.png"
+          alt="doodle"
+          className="doodle doodleI"
+        />
+        <img
+          src="/sigh/sigh_doodle_10.png"
+          alt="doodle"
+          className="doodle doodleJ"
+        />
+      </div>
+    </div>
+  );
 }
