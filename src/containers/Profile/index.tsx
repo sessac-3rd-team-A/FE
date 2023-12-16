244dc8b386fa9cb0724d64724d88335738a53f02
taehoon <mysoul98@gmail.com>
2023-12-16 20:34:12 +0900

    design: 프로필 페이지 왼쪽 스타일링

--- src/containers/Profile/index.tsx
+++ src/containers/Profile/index.tsx
@@ -1,4 +1,3 @@
-import { useParams } from 'next/navigation';
 import EmoCalendar from './EmoCalendar';
 import '@/styles/profile/index.scss';
 import Link from 'next/link';
@@ -9,19 +8,34 @@ export default function ProfilePage() {
   return (
     <section className="profile-container">
       <div className="profile-left">
-        <p>
+        <p className="title">
           행복한 쿼카님, <br /> 오늘의 기분은 어떠신가요?
         </p>
         <div className="left-content">
           <div className="count">
             <div>29</div>
-            <div>THE NUMBER OF SIGHS</div>
+            <div>
+              <p>THE NUMBER OF</p>
+              <p>SIGHS</p>
+            </div>
           </div>
           <Link href="/">
             <div className="link">한숨 쉬러 가기</div>
           </Link>
-          <div className="ratio">MOOD RATIO</div>
-          <div className="graph">MOOD GRAPH</div>
+          <div className="ratio">
+            {/* graph */}
+            <span>monthly</span>
+            <div>
+              <p>MOOD</p>
+              <p>RATIO</p>
+            </div>
+          </div>
+          <div className="graph">
+            <div>
+              <p>MOOD</p>
+              <p>GRAPH</p>
+            </div>
+          </div>
         </div>
       </div>
       <div className="profile-right">
