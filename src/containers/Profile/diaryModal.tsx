a2571e7bc0f4ab9a3fca3ff9594bc91efbe19883
Taehoon <mysoul98@gmail.com>
2023-12-19 15:28:51 +0900

    design: 프로필 페이지 캘린더 toSize 대신 rem으로 스타일링

--- src/containers/Profile/diaryModal.tsx
+++ src/containers/Profile/diaryModal.tsx
@@ -6,12 +6,14 @@ export default function DiaryModal() {
   return (
     <div className="diaryModal-container">
       <div className="date">2023.12.08</div>
-      <Image
-        src="/images/profileShop_background.jpg"
-        alt="picture diary"
-        width={300}
-        height={400}
-      />
+      <div className="img">
+        <Image
+          src="/images/profileShop_background.jpg"
+          alt="picture diary"
+          fill
+          sizes="(max-width: 768px) 50vw, (max-width: 1024px) 100vw"
+        />
+      </div>
     </div>
   );
 }
