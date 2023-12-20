17ed31c0ffa39b12c81512255588a466e834d238
jjsh03 <139740067+jjsh03@users.noreply.github.com>
2023-12-20 15:18:27 +0900

    feat: result sentiment 차트 제작

--- src/containers/Sigh/result.tsx
+++ src/containers/Sigh/result.tsx
@@ -1,6 +1,8 @@
 'use client';
-
+import Image from 'next/image';
 import '@/styles/sigh/result.scss';
+import { useEffect, useState } from 'react';
+import resultDoodle from '/public/sigh/result_doodle_1.png';
 
 export default function SighResultPage() {
   type resultType = {
@@ -22,11 +24,22 @@ export default function SighResultPage() {
     confidence: {
       negative: 97.326324,
       positive: 0.29289702,
-      neutral: 0.29289702,
+      neutral: 2.3807814,
     },
   };
 
-  console.log(sighResult);
+  const [negativeData, setNegativeData] = useState<number>();
+  const [positiveData, setPositiveData] = useState<number>();
+  const [neutralData, setNeutralData] = useState<number>();
+
+  useEffect(() => {
+    if (sighResult.confidence) {
+      const roundToInteger = (value: number) => Math.round(value);
+      setNegativeData(roundToInteger(sighResult.confidence.negative));
+      setPositiveData(roundToInteger(sighResult.confidence.positive));
+      setNeutralData(roundToInteger(sighResult.confidence.neutral));
+    }
+  }, [sighResult.confidence]);
 
   return (
     <div className="resultContainer">
@@ -45,7 +58,59 @@ export default function SighResultPage() {
         </section>
         <section className="resultSection resultSentiment">
           <h3>SENTIMENT</h3>
-          <article className="resultMemeArticle"></article>
+          <article className="resultSentimentArticle">
+            <div className="resultSentimentLabel">
+              <label>Negative</label>
+              <label>Positive</label>
+              <label>Neutral</label>
+            </div>
+            <div className="resultSentimentChart">
+              <div className="resultSentimentData resultNegativeData">
+                <div
+                  style={{
+                    width: `${negativeData}%`,
+                  }}
+                />
+                <img
+                  src="/sigh/negative.svg"
+                  alt="😢"
+                  style={{
+                    left:
+                      negativeData >= 0 && negativeData < 3 ? '-1.5%' : '-3%',
+                  }}
+                />
+              </div>
+              <div className="resultSentimentData resultPositiveData">
+                <div
+                  style={{
+                    width: `${positiveData}%`,
+                  }}
+                />
+                <img
+                  src="/sigh/positive.svg"
+                  alt="😄"
+                  style={{
+                    left:
+                      positiveData >= 0 && positiveData < 3 ? '-1.5%' : '-3%',
+                  }}
+                />
+              </div>
+              <div className="resultSentimentData resultNeutralData">
+                <div
+                  style={{
+                    width: `${neutralData}%`,
+                  }}
+                />
+                <img
+                  src="/sigh/neutral.svg"
+                  alt="😐"
+                  style={{
+                    left: neutralData >= 0 && neutralData < 3 ? '-1.5%' : '-3%',
+                  }}
+                />
+              </div>
+            </div>
+          </article>
         </section>
         <section className="resultBtns">
           <button>RESTART</button>
@@ -56,11 +121,7 @@ export default function SighResultPage() {
         <p>AH-</p>
         <p>WHEW!</p>
       </div>
-      <img
-        src="/sigh/result_doodle_1.png"
-        alt="doodle"
-        className="resultDoodle"
-      />
+      <Image src={resultDoodle} alt="doodle" className="resultDoodle" />
     </div>
   );
 }
