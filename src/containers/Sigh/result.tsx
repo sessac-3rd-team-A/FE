6411d11581a79dd756174dea14b73fe091bbedbf
jjsh03 <aberrant03@gmail.com>
2023-12-21 04:31:10 +0900

    fix: pictureDiary type error 수정 중

--- src/containers/Sigh/result.tsx
+++ src/containers/Sigh/result.tsx
@@ -4,58 +4,72 @@ import '@/styles/sigh/result.scss';
 import { useEffect, useState } from 'react';
 import resultDoodle from '/public/sigh/result_doodle_1.png';
 import Link from 'next/link';
+import { sighResultType } from '@/types';
 
-export default function SighResultPage() {
-  type resultType = {
-    painting: string;
-    meme: string;
-    sentiment: string;
-    confidence: {
-      negative: number;
-      positive: number;
-      neutral: number;
-    };
-  };
+export default function SighResultPage({
+  sighResult,
+}: {
+  sighResult: sighResultType;
+}) {
+  // 임시데이터
+  // const sighResult: resultType = {
+  //   painting:
+  //     'https://mblogthumb-phinf.pstatic.net/MjAxODAxMThfNzEg/MDAxNTE2MjQ5ODE4MjEy.iLPbjW9BLeAjkI1ppclbNdn5kwzTW585Abkz7Qte7c4g.LntCl6JMg0i88zWyCUpQ_bTtcvQuitpFLL29CXgUl0sg.JPEG.qe4865/23.jpg?type=w800',
+  //   meme: 'https://media1.jjalkey.com/media/1536141519140-6143a3e3c9.gif',
+  //   sentiment: 'negative',
+  //   confidence: {
+  //     negative: 97.326324,
+  //     positive: 0.29289702,
+  //     neutral: 2.3807814,
+  //   },
+  // };
 
-  const sighResult: resultType = {
-    painting:
-      'https://mblogthumb-phinf.pstatic.net/MjAxODAxMThfNzEg/MDAxNTE2MjQ5ODE4MjEy.iLPbjW9BLeAjkI1ppclbNdn5kwzTW585Abkz7Qte7c4g.LntCl6JMg0i88zWyCUpQ_bTtcvQuitpFLL29CXgUl0sg.JPEG.qe4865/23.jpg?type=w800',
-    meme: 'https://media1.jjalkey.com/media/1536141519140-6143a3e3c9.gif',
-    sentiment: 'negative',
-    confidence: {
-      negative: 97.326324,
-      positive: 0.29289702,
-      neutral: 2.3807814,
-    },
-  };
+  //   {
+  //     "id": 9,
+  //     "userId": "d1426c08-75ad-4957-8739-80a2ad60eeb6",
+  //     "pictureDiary": "https://example-bucket-seeun.s3.ap-northeast-2.amazonaws.com/3cba1799-3572-49e9-9f59-18cef2a8f9b8.jpeg",
+  //     "recommendedGif": "https://media1.jjalkey.com/link/1606606281599-e14c8a2348.jpg",
+  //     "sentiment": "neutral",
+  //     "positiveRatio": 0.43071458,
+  //     "negativeRatio": 0.14317966,
+  //     "neutralRatio": 99.42611,
+  //     "date": "2023-12-21"
+  // }
 
   const [negativeData, setNegativeData] = useState<number>(0);
   const [positiveData, setPositiveData] = useState<number>(0);
   const [neutralData, setNeutralData] = useState<number>(0);
 
   useEffect(() => {
-    if (sighResult.confidence) {
+    if (sighResult) {
       const roundToInteger = (value: number) => Math.round(value);
-      setNegativeData(roundToInteger(sighResult.confidence.negative));
-      setPositiveData(roundToInteger(sighResult.confidence.positive));
-      setNeutralData(roundToInteger(sighResult.confidence.neutral));
+      setNegativeData(roundToInteger(sighResult.positiveRatio));
+      setPositiveData(roundToInteger(sighResult.negativeRatio));
+      setNeutralData(roundToInteger(sighResult.neutralRatio));
     }
-  }, [sighResult.confidence]);
+    console.log(sighResult);
+  }, [sighResult]);
 
   return (
     <div className="result-container">
       <main className="result-mainContainer">
         <section className="result-section result-paint">
           <h3>TODAY'S AH-WHEW</h3>
-          <img
-            src={sighResult.painting}
-            alt="그림일기"
-            className="result-painting"
-          />
+          {sighResult.pictureDiary && (
+            <img
+              src={sighResult.pictureDiary}
+              alt="그림일기"
+              className="result-painting"
+            />
+          )}
         </section>
         <section className="result-section result-meme">
           <h3>MAYBE... YOU NEED THIS GIF</h3>
-          <img src={sighResult.meme} alt="짤" className="result-memeImg" />
+          <img
+            src={sighResult.recommendedGif}
+            alt="짤"
+            className="result-memeImg"
+          />
         </section>
         <section className="result-section result-sentiment">
           <h3>SENTIMENT</h3>
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,16 +1,30 @@
 export interface ShopApiRes {
-    title: string
-    link: string
-    image: string
-    lprice: number
-    hprice: number
-    mallName: string
-    productId: number
-    productType: number
-    brand: string
-    maker: string
-    category1: string
-    category2: string
-    category3: string
-    category4: string
+  title: string;
+  link: string;
+  image: string;
+  lprice: number;
+  hprice: number;
+  mallName: string;
+  productId: number;
+  productType: number;
+  brand: string;
+  maker: string;
+  category1: string;
+  category2: string;
+  category3: string;
+  category4: string;
 }
+
+// diary result
+export type sighResultType = {
+  id: number;
+  userId: string | null;
+  pictureDiary: string | undefined;
+  recommendedGif: string;
+  sentiment: string;
+  positiveRatio: number;
+  negativeRatio: number;
+  neutralRatio: number;
+  date: string;
+  // date: Date;
+};
