1a99ced11dd999e4049b3165bbb3ae12c74a046f
Sangwoo97 <aa6618@naver.com>
2023-12-21 23:17:34 +0900

    feat: 로그인 페이지 작업중 (#9)

--- src/containers/SignIn/index.tsx
+++ src/containers/SignIn/index.tsx
@@ -1,28 +1,91 @@
+'use client';
 import '@/styles/signIn/index.scss';
+import { FormEvent, useState } from 'react';
 
 export default function SignInPage() {
+  const [isLogin, setIslogin] = useState<boolean>(true);
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const apiUrl = 'http://localhost:8080/auth/signup';
+    const formData = new FormData(event.currentTarget);
+    const formattedData = Object.fromEntries(formData);
+    console.log(formattedData);
+    // const response = await fetch(apiUrl, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({
+    //     textDiary: sighText,
+    //   }),
+    // });
+
+    // Handle response if necessary
+    // const data = await response.json();
+    // ...
+  }
+
   return (
-    <div className="sign-mainContainer">
-      <article className="sign-contentContainer">
-        <p className="signIn-letter">SIGN IN</p>
-        <input
-          name="id"
-          type="text"
-          id="id"
-          placeholder="YOUR ID"
-          minLength={2}
-          maxLength={100}
-        />
-        <input
-          name="id"
-          type="text"
-          id="id"
-          className="pw"
-          placeholder="YOUR PASSWORD"
-          minLength={2}
-          maxLength={100}
-        />
-      </article>
-    </div>
+    <article className="sign-mainContainer">
+      <div
+        onClick={() => setIslogin(!isLogin)}
+        className="sign-checkboxContainer"
+      >
+        <div className={`sign-arrowlane ${isLogin ? '' : 'sign-register'}`}>
+          <img className="sign-arrow" src="/signIn/signArrow.svg" />
+        </div>
+      </div>
+      <div className="sign-contentContainer">
+        <div className={`flipper ${isLogin ? '' : 'flip'}`}>
+          <form className="sign-div front" onSubmit={onSubmit}>
+            <p className="signIn-letter">SIGN IN</p>
+            <input
+              name="id"
+              type="text"
+              id="id"
+              placeholder="YOUR ID"
+              minLength={2}
+              maxLength={100}
+            />
+            <input
+              name="pw"
+              type="text"
+              id="pw"
+              className="pw"
+              placeholder="YOUR PASSWORD"
+              minLength={2}
+              maxLength={100}
+            />
+            <button type="submit" className="submit-button">
+              SUBMIT
+            </button>
+          </form>
+
+          <form className="sign-div back" onSubmit={onSubmit}>
+            <p className="signIn-letter">SIGN</p>
+            <input
+              name="id"
+              type="text"
+              id="id"
+              placeholder="YOUR ID"
+              minLength={2}
+              maxLength={100}
+            />
+            <input
+              name="pw"
+              type="text"
+              id="pw"
+              className="pw"
+              placeholder="YOUR PASSWORD"
+              minLength={2}
+              maxLength={100}
+            />
+            <button type="submit" className="submit-button">
+              SUBMIT
+            </button>
+          </form>
+        </div>
+      </div>
+    </article>
   );
 }
