8bd2f5ff0c49bf72d49c74dadeab3e071d82b823
sy33002 <sy33002@naver.com>
2023-12-25 21:49:23 +0900

    feat: cookie token 확인중

--- src/containers/SignIn/index.tsx
+++ src/containers/SignIn/index.tsx
@@ -7,6 +7,7 @@ import { userState } from '@/utils/state'; // recoil
 import { useEffect } from 'react'; // recoil
 import MovingEye from '@/components/movingEye';
 import { useRafState } from 'react-use';
+import Cookies from 'js-cookie';
 
 export default function SignInPage() {
   const router = useRouter();
@@ -77,6 +78,10 @@ export default function SignInPage() {
       // 토큰 값은 로컬스토리지에 저장
       localStorage.setItem('accessToken', data.accessToken);
       localStorage.setItem('refreshToken', data.refreshToken);
+      // 토큰 값 쿠키에 저장
+      Cookies.set('accessToken', data.accessToken, { expires: 1 });
+      Cookies.set('refreshToken', data.refreshToken, { expires: 1 });
+      console.log('accessToken>>>',data);
 
       router.push('/');
     }
