28047db5f2008c12b9fb782f85ade653519abd53
jjsh03 <139740067+jjsh03@users.noreply.github.com>
2023-12-27 16:35:11 +0900

    chore: page import 수정

--- src/app/sigh/page.tsx
+++ src/app/sigh/page.tsx
@@ -1,11 +1,9 @@
 import SighPage from '@/containers/Sigh';
-import Error404 from '@/containers/common/error404';
 
 export default function Sigh() {
   return (
     <>
-      {/* <SighPage /> */}
-      <Error404 />
+      <SighPage />
     </>
   );
 }
