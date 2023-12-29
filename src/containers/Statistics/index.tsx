6ce929ab97c4d3c4e3838fe2a2e8243b30c5f308
22-JWL <100843910+22-JWL@users.noreply.github.com>
2023-12-29 18:56:30 +0900

    fix: 모바일 페이지 및 주석

--- src/containers/Statistics/index.tsx
+++ src/containers/Statistics/index.tsx
@@ -9,40 +9,7 @@ import ThreeBox from '@/containers/Statistics/threebox';
 // import { StatisticsTypeArray } from '@/types';
 
 export default async function StatisticsPage() {
-  // const selectedGender: string = 'F';
-  // const selectedAge: string = '20대';
   let statisticsInfo: any;
-  // let statisticsCategoryInfo: any;
-  // let memeImgInfo: any;
-  // const gender = (gen: string) => {
-  //   gen;
-  // };
-  // const age = (ag: string) => {
-  //   age;
-  // };
-
-  // let url = `${process.env.NEXT_PUBLIC_API_SERVER}/api/statistics/meme`;
-  // if (gender && age) {
-  //   url += `?gender=${gender}&age=${age}`;
-  // } else if (gender) {
-  //   url += `?gender=${gender}`;
-  // } else if (age) {
-  //   url += `?age=${age}`;
-  // }
-
-  //잘못했던 방법
-  // const fetchMemeImg = async () => {
-  //   const response = await fetch(
-  //     `${process.env.NEXT_PUBLIC_API_SERVER}/api/statistics/meme`,
-  //     {
-  //       cache: 'no-store',
-  //     },
-  //   );
-  //   memeImgInfo = await response.json();
-  //   // console.log(memeImgInfo);
-  // };
-
-  // fetchMemeImg();
 
   const statisticsResponse = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER}/api/statistics`,
@@ -51,34 +18,13 @@ export default async function StatisticsPage() {
     },
   );
   statisticsInfo = await statisticsResponse.json();
-  // console.log(statisticsInfo);
-
-  // const statisticsCategoryResponse = await fetch(
-  //   `${process.env.NEXT_PUBLIC_API_SERVER}/api/statistics?gender=${selectedGender}&age=${selectedAge}`,
-  //   {
-  //     cache: 'no-store',
-  //   },
-  // );
-  // statisticsCategoryInfo = await statisticsCategoryResponse.json();
-
-  // const memeImgInfoResponse = await fetch(
-  //   url, {
-  //   cache: 'no-store',
-  // });
-  // memeImgInfo = await memeImgInfoResponse.json();
 
   return (
     <div className="trend-container">
       <div className="chart-title">
         <Image src={trendText} alt="Picture of me" className="chart-text" />
       </div>
-      <ThreeBox
-        statisticsInfo={statisticsInfo}
-        // statisticsCategoryInfo={statisticsCategoryInfo}
-        // memeImgInfo={memeImgInfo}
-        // gender={gender}
-        // age={age}
-      />
+      <ThreeBox statisticsInfo={statisticsInfo} />
       <div className=" animate__animated animate__shakeY animate__infinite">
         <Semo />
       </div>
