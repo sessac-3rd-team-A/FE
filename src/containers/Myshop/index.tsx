5698e1053d39d372475348d9c0481fc56bd31213
sy33002 <sy33002@naver.com>
2023-12-31 22:46:29 +0900

    feat: content type

--- src/containers/Myshop/index.tsx
+++ src/containers/Myshop/index.tsx
@@ -66,7 +66,7 @@ export default function MyShopPage(): JSX.Element {
         {
           method: 'GET',
           headers: {
-            // 'Content-Type': 'application/json',
+            'Content-Type': 'application/json',
             Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
           },
         },
